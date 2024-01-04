83df910647c38b45ee21a720407d28119b97fe48
Erika Mayara Pimentel <tiaerikadev@gmail.com>
2024-01-04 00:32:00 -0300

    toogleable isFaceUp

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,27 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
             CardView()
-            CardView(isFaceUp: true)
             CardView()
-        }
-        .foregroundColor(.orange)
-        .padding()
-        
-        HStack {
-            CardView(isFaceUp: true)
             CardView()
-            CardView(isFaceUp: true)
             CardView()
         }
         .foregroundColor(.orange)
         .padding()
         
         HStack {
-            CardView(isFaceUp: true)
             CardView()
-            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+        
+        HStack {
+            CardView()
+            CardView()
+            CardView()
             CardView()
         }
         
@@ -40,20 +40,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
-        ZStack(content: {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.orange)
+                base.fill()
             }
-        })
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
